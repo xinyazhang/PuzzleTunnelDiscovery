72cc77b8c67efa5d32e172afa731ed235beabcce
Xinya Zhang <xinyazhang@utexas.edu>
2016-12-28 18:35:02 -0600

    octbuilder: fix the periodicity of Euler angles.
    Now it uses yaw, pitch and roll conventions, as the following
    Rotation Matrix = R(yaw=psi)*R(pitch=phi)*R(roll=theta).

--- src/octbuilder/space.h
+++ src/octbuilder/space.h
@@ -7,30 +7,46 @@ template<int ND, typename FLOAT>
 class TranslationWithEulerAngleGroup {
 	typedef Eigen::Matrix<FLOAT, ND, 1> Coord;
 	static constexpr int TaitBryanThetaIndex = 3;
+	static constexpr int ThetaIndex = 3; // Roll
+	static constexpr int PhiIndex = 4; // Pitch
+	static constexpr int PsiIndex = 5; // Yaw
 public:
+	// [-Pi, Pi)
+	static void round_into_2pi(double& angle)
+	{
+		// The least error-prone way ...
+		// (Maybe also faster in our cases?)
+		// FIXME: smarter way to do this.
+		while (angle < -M_PI)
+			angle += 2*M_PI;
+		while (angle >= M_PI)
+			angle -= 2*M_PI;
+	}
+
 	static Coord transist(const Coord& center, const Coord& delta)
 	{
 		// FIXME: check the correctness
 		Coord ret = center;
 		ret += delta;
-		if (ret(TaitBryanThetaIndex) > M_PI/2.0) {
-			ret(TaitBryanThetaIndex) = M_PI - ret(TaitBryanThetaIndex);
+		double theta = ret(ThetaIndex);
+		double phi = ret(PhiIndex);
+		double psi = ret(PsiIndex);
+		if (phi > M_PI/2.0) {
+			phi = M_PI - phi;
 			ret(TaitBryanThetaIndex + 1) += M_PI;
 			ret(TaitBryanThetaIndex + 2) += M_PI;
-		} else if (ret(TaitBryanThetaIndex) < -M_PI/2.0) {
-			ret(TaitBryanThetaIndex) = - ret(TaitBryanThetaIndex) - M_PI;
+		} else if (phi < -M_PI/2.0) {
+			phi = - phi - M_PI;
 			ret(TaitBryanThetaIndex + 1) += M_PI;
 			ret(TaitBryanThetaIndex + 2) += M_PI;
 		}
-		round_into_2pi(ret(TaitBryanThetaIndex + 1));
-		round_into_2pi(ret(TaitBryanThetaIndex + 2));
-		return ret;
-	}
 
-	static void round_into_2pi(double& val)
-	{
-		int r(std::floor(val / (2 * M_PI)));
-		val -= r * (2 * M_PI);
+		round_into_2pi(theta);
+		round_into_2pi(psi);
+		ret(ThetaIndex) = theta;
+		ret(PhiIndex) = phi;
+		ret(PsiIndex) = psi;
+		return ret;
 	}
 };
 
