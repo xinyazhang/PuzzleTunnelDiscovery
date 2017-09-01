ba1e0b251a698e3710ca7334dd0937ad812f59f8
Xinya Zhang <xinyazhang@utexas.edu>
2017-09-01 14:57:34 -0500

    omplaux/map: finish readMap function.
    This function recognize the OBJ file format with the following modifications:
    	* v indicates a robot configuration (vertex).
    	* e indicates connections b/w two configurations (edge).
    	* b indicates a broken connection b/w two configurations.
    After reading the roadmap, this function drops vertices and edges that
    is disconnected from Vertex Zero, and return the processed vertices and
    edges.

--- lib/omplaux/map.cc
+++ lib/omplaux/map.cc
@@ -1,11 +1,15 @@
 #include "map.h"
 #include <fstream>
+#include <queue>
+#include <map>
+#include <iostream>
 
 namespace omplaux {
 
 void Map::readMap(const std::string& fn)
 {
 	std::ifstream fin(fn);
+	std::map<int, std::vector<int>> edges;
 	while (true) {
 		char c;
 		fin >> c;
@@ -20,11 +24,64 @@ void Map::readMap(const std::string& fn)
 			double qx, qy, qz, qw;
 			fin >> qx >> qy >> qz >> qw;
 			Q.emplace_back(qw, qx, qy, qz);
-		} else if (c == 'e') {
+		} else if (c == 'e' || c == 'b') {
 			int v1, v2;
 			fin >> v1 >> v2;
-			E.emplace_back(v1, v2);
+			if (c == 'e') {
+				E.emplace_back(v1, v2);
+				edges[v1].emplace_back(v2);
+			}
 		}
+	}
+	std::vector<int> old2new(T.size(), 0);
+	std::queue<int> queue;
+	queue.push(1);
+	old2new[1] = 1;
+	// locate vertices to keep
+	// 1: keep
+	// 0: drop (this is default)
+	while (!queue.empty()) {
+		int v1 = queue.front();
+		queue.pop();
+		for (int v2 : edges[v1]) {
+			if (old2new[v2])
+				continue;
+			queue.push(v2);
+			old2new[v2] = 1;
+		}
+	}
+	int newidx = 0;
+	for (int i = 0; i < int(T.size()); i++) {
+		if (old2new[i])
+			old2new[i] = newidx++;
+		else
+			old2new[i] = -1;
+	}
+	std::cerr << "total vertices left: " << newidx << std::endl;
+	for (auto& e : E) {
+		int nv1 = old2new[e.first];
+		int nv2 = old2new[e.second];
+		e = std::pair<int, int>(nv1, nv2);
+	}
+	int i = 0;
+	for (auto it = T.begin(); it != T.end(); i++) {
+		if (old2new[i] < 0)
+			it = T.erase(it);
+		else
+			it++;
+	}
+	i = 0;
+	for (auto it = Q.begin(); it != Q.end(); i++) {
+		if (old2new[i] < 0)
+			it = Q.erase(it);
+		else
+			it++;
+	}
+	for (auto it = E.begin(); it != E.end(); ) {
+		if (it->first < 0 || it->second < 0)
+			it = E.erase(it);
+		else
+			it++;
 	}
 }
 
