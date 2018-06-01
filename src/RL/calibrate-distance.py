7ab026aca61ef5cb967479273a4acbb43e535ee4
Xinya Zhang <xinyazhang@utexas.edu>
2018-06-01 16:42:02 -0500

    RL/calibrate-distance.py: remote distant samples to sanitize the training.

--- src/RL/calibrate-distance.py
+++ src/RL/calibrate-distance.py
@@ -19,15 +19,26 @@ def calibrate(gtfn, cfn):
     r.angleModel(0.0, 0.0)
     r.default_depth = 0.0
     r.views = np.array([[0.0, 0.0]], dtype=np.float32)
+    origin = np.array([0,0,0,1,0,0,0], dtype=np.float64)
     d = np.load(gtfn)
     V=d['V']
     D=d['D']
-    neg=[]
+    pos = []
+    posd = []
+    neg = []
     negd = []
     for i in range(len(V)):
         V[i] = r.translate_to_unit_state(V[i])
         if r.is_disentangled(V[i]):
             D[i] = 0.0
+    # Trim distant samples, which usually shows nothing
+    for i in range(len(V)):
+        if pyosr.distance(origin, V[i]) > 0.75:
+            continue
+        pos.append(V[i])
+        posd.append(D[i])
+    V = np.array(pos)
+    D = np.array(posd)
     for i in range(len(V)):
         while True:
             s = uw_random.random_state()
