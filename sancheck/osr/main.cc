8b8df7c8d25eee74e60541cf1831223d14505874
Xinya Zhang <xinyazhang@utexas.edu>
2018-06-23 18:00:52 -0600

    sancheck/osr: add multi-threading test.
    NVIDIA Driver 390.?? installed from Ubuntu 16.04 official repo has a bug: the
    main thread can render but worker threads segfaults in calling any GL function.
    
    sancheck_osr now checks against this case.

--- sancheck/osr/main.cc
+++ sancheck/osr/main.cc
@@ -6,6 +6,23 @@
 #include <fstream>
 #include <string.h>
 #include <glm/ext.hpp>
+#include <thread>
+
+void worker(char* argv[])
+{
+	auto dpy = osr::create_display();
+	if (dpy == EGL_NO_DISPLAY) {
+		printf("WORKER THREAD CANNOT CREATE DISPLAY\n");
+	}
+	osr::create_gl_context(dpy);
+	osr::Renderer renderer;
+	renderer.setup();
+	renderer.loadModelFromFile(argv[1]);
+	renderer.angleModel(30.0f, 30.0f);
+	std::ofstream fout("osrsc_worker.bin");
+	renderer.render_depth_to(fout);
+	fout.close();
+}
 
 
 int main(int argc, char *argv[])
@@ -16,7 +33,6 @@ int main(int argc, char *argv[])
 		return -1;
 	}
 	osr::create_gl_context(dpy);
-	osr::shutdown();
 	osr::Renderer renderer;
 	renderer.setup();
 	renderer.loadModelFromFile(argv[1]);
@@ -24,7 +40,13 @@ int main(int argc, char *argv[])
 	std::ofstream fout("osrsc.bin");
 	renderer.render_depth_to(fout);
 	fout.close();
+	printf("MAIN THREAD TESTING FINISHED\n");
+	std::thread t1(worker, argv);
+
+	t1.join();
+	printf("WORKER THREAD TESTING FINISHED\n");
 	renderer.teardown();
-	printf("PASSED\n");
+	osr::shutdown();
+	printf("SHUTDOWN\n");
 	return 0;
 }
