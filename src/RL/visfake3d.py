8cf0a063fac7ad3f257f54d440931b6ac251cc9b
Xinya Zhang <xinyazhang@utexas.edu>
2018-07-16 18:45:30 -0500

    RL/visfake3d: visualize fake3d samples.

---
+++ src/RL/visfake3d.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+fn = sys.argv[1]
+d = np.load(fn)
+if 'Q' not in d:
+    # V,D dataset
+    Q = d['V']
+    V = d['D']
+else:
+    # Q,V dataset
+    Q = d['Q']
+    V = d['V']
+
+xs = Q[:, 0]
+ys = Q[:, 1]
+zs = V
+ax.scatter(xs, ys, zs, c=V)
+
+ax.set_xlabel('X Label')
+ax.set_ylabel('Y Label')
+ax.set_zlabel('Z Label')
+
+plt.show()
