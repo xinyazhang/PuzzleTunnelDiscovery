e2d13a85be076a4442678f0e939e77976d3cffac
Xinya Zhang <xinyazhang@utexas.edu>
2018-08-07 19:12:13 -0500

    RL/a2c_overfit: now we estimate V* by assuming other trajectories always yield 0 rewards.

--- src/RL/a2c_overfit.py
+++ src/RL/a2c_overfit.py
@@ -37,8 +37,38 @@ class OverfitTrainer(A2CTrainer):
                 train_everything=train_everything)
         self.all_samples_cache = []
         self.minibatch_index = 0
+        self.num_actions = len(advcore.args.actionset)
+
+    # Redefine actor critic function, details:
+    # 1. We are trying to overfit the model through a known optimal path
+    # 2. Hence we can't calculate V* since Monte Carlo part was removed
+    # 3. Therefore we can assume every other trajectory yields 0 long term rewards
+    # 4. Thus, V* = V_{opt} times 1/total number of actions ^ (remaining length of this trajectory),
+    #    since at each step, the probability to take the optimal action is 1/total number of actions
+    # This requires a different loss function and different set of inputs
+    def build_loss(self, advcore):
+        if self.loss is not None:
+            return self.loss
+        self.Adist_tensor = advcore.action_tensor
+        # TD is required
+        # V_sample - V*, for this case it's V_opt - V*
+        self.V_star = tf.placeholder(tf.float32, shape=[None], name='V_star_ph')
+        self.V_tensor = tf.placeholder(tf.float32, shape=[None], name='VPh')
+
+        flattened_value = tf.reshape(advcore.value, [-1])
+        policy = tf.multiply(advcore.softmax_policy, self.Adist_tensor)
+        policy = tf.reduce_sum(policy, axis=[1,2])
+        log_policy = tf.log(tf.clip_by_value(policy, 1e-20, 1.0))
+        policy_per_sample = log_policy * tf.stop_gradient(self.V_tensor - flattened_value)
+        criticism = self.V_tensor - flattened_value
+        policy_loss = tf.reduce_sum(-policy_per_sample)
+        value_loss = tf.nn.l2_loss(self.V_star - flattened_value)
+
+        self.loss = policy_loss + value_loss
+        return self.loss
 
     def train(self, envir, sess, tid=None, tmax=-1):
+        advcore = self.advcore
         if not self.all_samples_cache:
             print("Caching samples")
             while True:
@@ -46,17 +76,68 @@ class OverfitTrainer(A2CTrainer):
                 self.all_samples_cache.append((rlsamples, final_state))
                 if rlsamples[-1].reaching_terminal:
                     break
-        rlsamples, final_state = self.all_samples_cache[self.minibatch_index % len(self.all_samples_cache)]
+        (rlsamples, final_state) = self.all_samples_cache[self.minibatch_index % len(self.all_samples_cache)]
+        assert isinstance(rlsamples[0], rlenv.RLSample), "rlsamples[0] is not RLSample"
+        assert isinstance(final_state, rlenv.RLSample), "final_state is not RLSample"
         self.minibatch_index += 1 # [DISABLED] HACKING: NO SECOND SEGMENT
-        self.a2c(envir=envir,
-                 sess=sess,
-                 vstates=[s.vstate for s in rlsamples] + [final_state.vstate],
-                 action_indices=[s.action_index for s in rlsamples],
-                 ratios=[s.ratio for s in rlsamples],
-                 rewards=[s.combined_reward for s in rlsamples],
-                 values=None,
-                 reaching_terminal=rlsamples[-1].reaching_terminal
-                )
+
+        # Collect image and action input
+        vstates = [s.vstate for s in rlsamples] + [final_state.vstate]
+        # print('{}'.format(rlsamples[0].vstate[0].shape))
+        batch_rgb = [state[0] for state in vstates]
+        batch_dep = [state[1] for state in vstates]
+        action_indices=[s.action_index for s in rlsamples]
+        batch_adist = rlutil.actions_to_adist_array(action_indices, dim=self.action_space_dimension)
+
+        # Collect V* and V
+        if final_state.is_terminal:
+            V = 0.0
+        else:
+            V = np.asscalar(advcore.evaluate([final_state.vstate], sess, tensors=[advcore.value])[0])
+        V_star = V
+
+        r_rewards = [s.combined_reward for s in rlsamples][::-1]
+        batch_V = []
+        batch_V_star = []
+        GAMMA = self.gamma
+        KAPPA = 1.0 / self.num_actions
+        assert GAMMA > 0, "a2c_overfit does not support linear decay"
+        for r in r_rewards:
+            V = r + GAMMA * V
+            # Programmatically batch_V_star = batch_V * KAPPA
+            # We did this to elaborate the derivation of V_star
+            V_star = KAPPA * r + KAPPA * GAMMA * V
+            batch_V.append(V)
+            batch_V_star.append(V_star)
+        batch_V.reverse()
+        batch_V_star.reverse()
+
+        ndic = {
+                'rgb' : batch_rgb,
+                'dep' : batch_dep,
+                'aindex' : action_indices,
+                'adist' : batch_adist,
+                'V' : batch_V,
+                'Vstar' : batch_V_star,
+               }
+        self.dispatch_training(sess, ndic)
+
+    def dispatch_training(self, sess, ndic, debug_output=True):
+        advcore = self.advcore
+        dic = {
+                advcore.rgb_1: ndic['rgb'][:-1],
+                advcore.dep_1: ndic['dep'][:-1],
+                advcore.rgb_2: ndic['rgb'][1:],
+                advcore.dep_2: ndic['dep'][1:],
+                advcore.action_tensor : ndic['adist'],
+                self.V_star : ndic['Vstar'],
+                self.V_tensor: ndic['V']
+              }
+        if self.summary_op is not None:
+            _, summary, gs = sess.run([self.train_op, self.summary_op, self.global_step], feed_dict=dic)
+            self.train_writer.add_summary(summary, gs)
+        else:
+            sess.run(self.train_op, feed_dict=dic)
 
 '''
 Try to overfit V and P with given samples
