f03e62ec819fd7dcb59235f52b920978e3a700c6
Xinya Zhang <xinyazhang@utexas.edu>
2018-08-09 14:54:30 -0500

    RL/check_l2grad: check the magnitue of gradients from L2 loss.

---
+++ src/RL/check_l2grad.py
@@ -0,0 +1,19 @@
+
+import tensorflow as tf
+import numpy as np
+
+K = 4
+N = 512
+GT = np.array([[float(i) for i in range(K)]])
+V = tf.Variable(np.zeros(shape=(K)))
+
+loss = tf.nn.l2_loss(V-GT)
+optimizer = tf.train.AdamOptimizer(learning_rate=1e-2)
+train_op = optimizer.minimize(loss)
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    for i in range(N):
+        print(sess.run(V))
+        sess.run(train_op)
+    print(sess.run(V))
