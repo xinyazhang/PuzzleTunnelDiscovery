519cd4cdc89fbfa90a7df633e48442bea43ff5c7
Xinya Zhang <xinyazhang@utexas.edu>
2018-08-16 14:59:59 -0500

    RL/tunnel-segment-finder: given a known path, find the segments in the narrow tunnel.

---
+++ src/RL/tunnel-segment-finder.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python2
+
+import sys
+import numpy as np
+
+def tunnel_segment_finder(gtfn, vmfn, outfn):
+    gtdic = np.load(gtfn)
+    V = gtdic['V']
+    VM = np.load(vmfn)['VM']
+    VPS = np.sum(VM, axis=1) #Visibility per sample
+    OVPS = np.sort(VPS)
+    thresh = OVPS[int(len(VPS) / 5)]
+    tunnel_vi = []
+    for i,vb in enumerate(VPS):
+        if vb <= thresh:
+            tunnel_vi.append(i)
+    print(V[tunnel_vi])
+    np.savez(outfn, TUNNELV=V[tunnel_vi])
+
+def usage():
+    print('''
+Find least visible samples
+    ''')
+    print('''
+Usage: tunnel-finder.py <npz file from rl-precalcmap.py> <npz file from visibility-filder> <output tunnel sample npz>
+'''
+    )
+
+if __name__ == '__main__':
+    #tunnel_finder(sys.argv[1], sys.argv[2], sys.argv[3])
+    tunnel_segment_finder('blend-low.gt.npz',
+                          '../res/alpha/alpha-1.2.org.w-first.npz',
+                          'blend-low.tunnel-segment.npz')
