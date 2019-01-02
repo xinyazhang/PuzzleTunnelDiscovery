d5fc56ef11963de51fce0bdc7372083b20f9a168
Xinya Zhang <xinyazhang@utexas.edu>
2019-01-01 19:22:30 -0600

    GP/dualconf_tiny: always use the geometry with texture coordinates.
    To make everything consistent. Check doc/interop-with-ompl.md for more
    details.

--- src/GP/dualconf_tiny.py
+++ src/GP/dualconf_tiny.py
@@ -1,7 +1,9 @@
 import numpy as np
 
-env_fn = '../res/dual/dual_tiny.dt.obj'
-rob_fn = '../res/dual/knotted_ring.dt.obj'
+#env_fn = '../res/dual/dual_tiny.dt.obj'
+#rob_fn = '../res/dual/knotted_ring.dt.obj'
+env_fn = '../res/dual/dual_tiny.dt.tcp.obj'
+rob_fn = '../res/dual/knotted_ring.dt.tcp.obj'
 keys_fn = '../res/dual/dual_tiny.path'
 keys_w_last = True
 # keys_fn = 'blend.path'
@@ -10,8 +12,8 @@ keys_w_last = True
 # NOTE: DO NOT LOAD OBJ WITH UV, OTHERWISE IT CREATES DUPLICATED VERTICES
 env_wt_fn = env_fn
 rob_wt_fn = rob_fn
-env_uv_fn = '../res/dual/dual_tiny.dt.tcp.obj'
-rob_uv_fn = '../res/dual/knotted_ring.dt.tcp.obj'
+env_uv_fn = env_fn
+rob_uv_fn = rob_fn
 rob_ompl_center = None
 # tunnel_v_fn = None # Unknown yet, just a placeholder during the preprocessing.
 tunnel_v_fn = '../res/dual/dual_tiny.tunnel-manual.npz' # Pickup the tunnel vertices manually
