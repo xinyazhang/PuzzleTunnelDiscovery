714b139ddd1d984b4b793752b55b501d590d90bf
Xinya Zhang <xinyazhang@utexas.edu>
2019-01-02 21:47:21 -0600

    GP/npz_mean: read npz files storing similar np.arrays and calculate the mean of them.

---
+++ src/GP/npz_mean.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import os
+import sys
+
+def mean(fns):
+    dsum = {}
+    for fn in fns:
+        d = np.load(fn)
+        for k,v in d.items(): # Python 3 syntax
+            if k not in dsum:
+                dsum[k] = v
+            else:
+                dsum[k] += v
+    denom = float(len(fns))
+    for k,v in dsum.items():
+        dsum[k] /= denom
+    return dsum
+
+def main(files):
+    ifns = files[:-1]
+    ofn = files[-1]
+    dic = mean(ifns)
+    np.savez(ofn, **dic)
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
