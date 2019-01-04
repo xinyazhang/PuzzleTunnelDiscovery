943eb55d04d7751bd04aa7b82ff68cf1aeb6964a
Xinya Zhang <xinyazhang@utexas.edu>
2019-01-03 18:37:14 -0600

    GP,hg: add dualconf.py

---
+++ src/GP/dualconf.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+#env_fn = '../res/dual/dual_tiny.dt.obj'
+#rob_fn = '../res/dual/knotted_ring.dt.obj'
+env_fn = '../res/dual/dual.dt.tcp.obj'
+rob_fn = '../res/dual/knotted_ring.dt.tcp.obj'
+keys_fn = None # Testing data has no path
+keys_w_last = True
+
+env_wt_fn = env_fn
+rob_wt_fn = rob_fn
+env_uv_fn = env_fn
+rob_uv_fn = rob_fn
+rob_ompl_center = None
+tunnel_v_fn = None # Testing data has no tunnel V
