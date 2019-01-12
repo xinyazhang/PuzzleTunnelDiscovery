df4c51241c1a82d4d27ccc41d2d7969b8846c910
Xinya Zhang <xinyazhang@utexas.edu>
2019-01-11 19:16:42 -0600

    GP,hg: move dualconf_g4 from hg to GP and symlink back to hg
    The configuration files are supposed to located at GP (geometry
    processing).

---
+++ src/GP/dualconf_g4.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+#env_fn = '../res/dual/dual_tiny.dt.obj'
+#rob_fn = '../res/dual/knotted_ring.dt.obj'
+env_fn = '../res/dual/dual-g4.dt.tcp.obj'
+rob_fn = '../res/dual/knotted_ring.dt.tcp.obj'
+keys_fn = None
+keys_w_last = True
+# keys_fn = 'blend.path'
+# keys_w_last = False
+
+env_wt_fn = env_fn
+rob_wt_fn = rob_fn
+env_uv_fn = env_fn
+rob_uv_fn = rob_fn
+rob_ompl_center = None
+tunnel_v_fn = None
