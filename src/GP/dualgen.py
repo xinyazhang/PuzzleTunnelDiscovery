c02b56462c277bf1db99996fc800671fdcea4c9f
Xinya Zhang <xinyazhang@utexas.edu>
2019-03-07 17:14:22 -0600

    GP/dualgen: add --debug option to print the mesh without applying the boolean operations.

--- src/GP/dualgen.py
+++ src/GP/dualgen.py
@@ -44,7 +44,11 @@ def pos_to_carve_origin(pos, tangent, normal, up, is_top=True):
     return carve_origin
 
 def build_stick_x(desc):
+    is_debug = desc['debug']
     V, F = create_cube(desc['len'], STICK_WIDTH, STICK_HEIGHT)
+    origin = np.array(list(desc['origin'])+[0.0], dtype=np.float64)
+    # print(desc)
+    # print(origin)
     # Template at origin
     VT, FT = create_cube(FILLISTER_LENGTH,
                          STICK_WIDTH+2*FILLISTER_MARGIN, # Both sides
@@ -52,24 +56,34 @@ def build_stick_x(desc):
     tangent = np.array([1,0,0], dtype=np.float64)
     normal = np.array([0,1,0], dtype=np.float64)
     up = np.array([0,0,1], dtype=np.float64)
+    Mpos = [(V+origin, F)]
+    # print('Mpos V {}'.format(V+origin))
+    Mneg = []
     for pos in desc['up']:
         carve_origin = pos_to_carve_origin(pos, tangent, normal, up, is_top=True)
         VC = VT + carve_origin
-        V, F = pyosr.mesh_bool(V, F, VC, FT, pyosr.MESH_BOOL_MINUS)
+        if is_debug:
+            # print('Mneg V {}'.format(VC+origin))
+            Mneg.append((VC+origin, FT))
+        else:
+            V, F = pyosr.mesh_bool(V, F, VC, FT, pyosr.MESH_BOOL_MINUS)
     # FIXME: Dedup the code
     for pos in desc['down']:
         carve_origin = pos_to_carve_origin(pos, tangent, normal, up, is_top=False)
         #print("carve_origin {}".format(carve_origin))
         VC = VT + carve_origin
-        V, F = pyosr.mesh_bool(V, F, VC, FT, pyosr.MESH_BOOL_MINUS)
-    origin = np.array(list(desc['origin'])+[0.0], dtype=np.float64)
+        if is_debug:
+            # print('V {}'.format(V+origin))
+            Mneg.append((VC+origin, FT))
+        else:
+            V, F = pyosr.mesh_bool(V, F, VC, FT, pyosr.MESH_BOOL_MINUS)
     V += origin
-    return V, F
+    if is_debug:
+        return Mpos, Mneg
+    else:
+        return V, F
 
-def build_stick_y(desc):
-    desc_x = dict(desc)
-    desc_x['origin'] = (desc['origin'][1], desc['origin'][0]) # Pretend it's arrange in X direction
-    V_x, F_x = build_stick_x(desc_x)
+def translate_from_x_to_y(V_x, F_x):
     V = np.copy(V_x)
     F = np.copy(F_x)
     # Swap X and Y
@@ -79,6 +93,24 @@ def build_stick_y(desc):
     F[:, 0] = F_x[:,1]
     F[:, 1] = F_x[:,0]
     return V, F
+
+def build_stick_y(desc):
+    is_debug = desc['debug']
+    desc_x = dict(desc)
+    desc_x['origin'] = (desc['origin'][1], desc['origin'][0]) # Pretend it's arrange in X direction
+    V_x, F_x = build_stick_x(desc_x)
+    if is_debug:
+        Mpos_x, Mneg_x = V_x, F_x
+        Mpos = []
+        Mneg = []
+        for V,F in Mpos_x:
+            Mpos.append(translate_from_x_to_y(V, F))
+            print("Flip pos {}".format(Mpos[-1][1]))
+        for V,F in Mneg_x:
+            Mneg.append(translate_from_x_to_y(V, F))
+            print("Flip neg {}".format(Mneg[-1][1]))
+        return Mpos,Mneg
+    return translate_from_x_to_y(V_x, F_x)
 
 def merge_mesh(mesh_pair):
     mesh_0, mesh_1 = mesh_pair
@@ -92,19 +124,70 @@ def merge_mesh(mesh_pair):
     '''
     return V,F
 
+def multi_save(meshes, fn, color):
+    Vall = None
+    Fall = None
+    for V,F in meshes:
+        Vall = V if Vall is None else np.concatenate((Vall, V), axis=0)
+        # print("FT {}".format(F))
+        F = np.copy(F) + Vall.shape[0] - V.shape[0]
+        # print("F delta {}".format(Vall.shape[0] - V.shape[0]))
+        Fall = F if Fall is None else np.concatenate((Fall, F), axis=0)
+    empty = np.empty(shape=(0, 0))
+    color = color.reshape((1,3))
+    Vc = np.repeat(color, Vall.shape[0], axis=0)
+    print(Vall.shape)
+    print(Vc.shape)
+    V = np.concatenate((Vall, Vc), axis=1)
+    F = Fall
+    pyosr.save_obj_1(V, F, fn)
+
 def main():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('pkg', help='Package name', nargs=None, type=str)
+    parser.add_argument('--debug', help='Enable debugging output', action='store_true')
     args = parser.parse_args()
     pkg = getattr(dualdata, args.pkg)
 
-    p = multiprocessing.Pool(8)
+    # p = multiprocessing.Pool(8)
+    p = multiprocessing.Pool(1)
+    for i,e in enumerate(pkg.STICKS_X_DESC):
+        e['debug'] = args.debug
+        e['bar_id'] = i
+    for i,e in enumerate(pkg.STICKS_Y_DESC):
+        e['debug'] = args.debug
+        e['bar_id'] = i
     print(pkg.STICKS_X_DESC)
+    print(pkg.STICKS_Y_DESC)
+    '''
+    meshes_x = []
+    for desc in pkg.STICKS_X_DESC:
+        meshes_x.append(build_stick_x(desc))
+    '''
     meshes_x = p.map(build_stick_x, pkg.STICKS_X_DESC)
+    # print('Mpos {}'.format(meshes_x[0][0]))
+    # print('Mneg {}'.format(meshes_x[0][1]))
     # meshes_x = []
+    '''
+    meshes_y = []
+    for desc in pkg.STICKS_Y_DESC:
+        meshes_y.append(build_stick_y(desc))
+    '''
     meshes_y = p.map(build_stick_y, pkg.STICKS_Y_DESC)
     # meshes_y = []
     meshes = meshes_x + meshes_y
+    if args.debug:
+        Mpos = []
+        Mneg = []
+        for i,e in enumerate(meshes):
+            #print("{}: {}".format(i, e))
+            pmesh, nmesh = e
+            Mpos += pmesh
+            Mneg += nmesh
+        # print(Mneg)
+        multi_save(Mpos, 'dual-pos.obj', color=np.array([0.0, 0.0, 0.75]))
+        multi_save(Mneg, 'dual-neg.obj', color=np.array([0.75, 0.0, 0.0]))
+        return
     # meshes = meshes[0:1] # Debug
     print(len(meshes))
     while len(meshes) > 1:
