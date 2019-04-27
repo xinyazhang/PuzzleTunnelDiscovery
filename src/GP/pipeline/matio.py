931115c5843f0f8d569494b5e74f88f6b54cadf8
Xinya Zhang <xinyazhang@utexas.edu>
2019-04-26 22:08:18 -0500

    GP/pipeline: matio.load now accepts pathlib.PosixPath
    The code is also refactored

--- src/GP/pipeline/matio.py
+++ src/GP/pipeline/matio.py
@@ -1,18 +1,25 @@
 import numpy as np
 import h5py
 from scipy.io import loadmat,savemat
+import pathlib
+
+def _load_csv(fn):
+    return np.loadtxt(fn, delimiter=',')
+
+def _load_hdf5(fn):
+    return h5py.File(fn, 'r')
+
+_SUFFIX_TO_LOADER = {
+        '.npz': np.load,
+        '.txt': np.loadtxt,
+        '.csv': _load_csv,
+        '.mat': loadmat,
+        '.hdf5': _load_hdf5
+        }
 
 def load(fn):
-    if fn.endswith('.npz'):
-        return np.load(fn)
-    elif fn.endswith('.txt'):
-        return np.loadtxt(fn)
-    elif fn.endswith('.csv'):
-        return np.loadtxt(fn, delimiter=',')
-    elif fn.endswith('.mat'):
-        return loadmat(fn)
-    elif fn.endswith('.hdf5'):
-        return h5py.File(fn, 'r')
+    p = pathlib.PosixPath(fn)
+    return _SUFFIX_TO_LOADER[p.suffix](fn)
 
 def hdf5_overwrite(f, path, ds):
     if path in f:
