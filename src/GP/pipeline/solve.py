670065db8d3d016b1612b9bfaccb8b4b08a63b93
Xinya Zhang <xinyazhang@utexas.edu>
2019-05-10 01:49:51 -0500

    GP/pipeline: use ws.keyconf_prediction_file to honor --current_trial in forest_rdt stage

--- src/GP/pipeline/solve.py
+++ src/GP/pipeline/solve.py
@@ -150,7 +150,11 @@ def forest_rdt(args, ws):
         # /u/zxy/applique/puzzle-geometry/puzzles/claw/claw.cfg 15 1.00
         # --samset mkobs-claw/pds/1m.0.npz --replace_istate
         # file=mkobs-claw/key.ompl.withig.npz,offset=$$([$(Process)*1]),size=1,out=mkobs-claw/trial-1/pdsrdt/
-        key_fn = ws.local_ws(util.TESTING_DIR, puzzle_name, util.KEY_PREDICTION)
+        #key_fn = ws.local_ws(util.TESTING_DIR, puzzle_name, util.KEY_PREDICTION)
+
+        # Note: no need to probe the key configuration, which has been done in sample_pds and 
+        #       ws.keyconf_prediction_file should always point to a valid keyconf
+        key_fn = ws.keyconf_prediction_file(puzzle_name)
         condor_job_args = ['se3solver.py',
                 'solve',
                 '--samset', _puzzle_pds(ws, puzzle_name, ws.current_trial),
