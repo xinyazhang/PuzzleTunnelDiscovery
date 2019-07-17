c76d456b6867ddacd0df09c42bb18f8841059622
Xinya Zhang <xinyazhang@utexas.edu>
2019-07-16 20:14:31 -0500

    GP/forest_dijkstra: add --bloom_dir so blooming trees can be connected in connect_forest stage.
    Note: this commit does not integrate this feature to the pipeline.

--- src/GP/forest_dijkstra.py
+++ src/GP/forest_dijkstra.py
@@ -34,7 +34,7 @@ def _load(fefn, ds_name='E'):
     return d[ds_name] if ds_name in d else None
 
 class TreePathFinder(object):
-    def __init__(self, root, ssc_fn, ct_fn, pds, pds_ids, pds_flags):
+    def __init__(self, root, ssc_fn, ct_fn, pds, pds_ids, pds_flags, bloom_range, bloom_fn):
         print("Loading data from {} {}".format(ssc_fn, ct_fn))
         self._root_conf = root
         self._ssc = loadmat(ssc_fn)['C'] # Note: no flatten for sparse matrix
@@ -45,11 +45,15 @@ class TreePathFinder(object):
         self._ct_edges = d['CE']
         self._pds = pds
         self._pds_flags = pds_flags
+        self._bloom_range = bloom_range
+        self._bloom_fn = bloom_fn
         G = self._G = nx.Graph()
         G.add_nodes_from([-1]) # One root
         G.add_nodes_from(pds_ids)
         # print(self._ct_edges[:20])
         G.add_edges_from(self._ct_edges)
+
+        self._bloom_G = None
 
     def get_node_conf(self, node):
         if node < 0:
@@ -78,7 +82,10 @@ class TreePathFinder(object):
                     leaf = n
                     # print('CT edges\n{}'.format(self._ct_edges))
                     break
+        if self.has_edge_to_pds(leaf):
+            return self.bloom_nodes_from_root(leaf)
         assert self._ssc[0, leaf] != 0
+        # compact tree uses -1 as the root, and 0,1,2,... are PDS nodes
         ids = nx.shortest_path(self._G, -1, leaf)
         print("IDs on the path {}".format(ids))
         ssc_on_path = []
@@ -89,6 +96,30 @@ class TreePathFinder(object):
                 ssc_on_path.append(self._ssc[0,i])
         print("SSC on the path {}".format(ssc_on_path))
         path = [self.get_node_conf(node) for node in ids]
+        return path[::-1]
+
+    def has_edge_to_pds(self, pds_id):
+        if self._bloom_range is None or self._bloom_fn is None:
+            return False
+        f, t = self._bloom_range
+        return f <= pds_id < t
+
+    def bloom_nodes_from_root(self, pds_id):
+        if self._bloom_G is None:
+            G = self._bloom_G = nx.Graph()
+            self._bloom_nodes = _load(self._bloom_fn, 'BLOOM')
+            n = self._bloom_nodes.shape[0]
+            bloom_edges = np.transpose(_load(self._bloom_fn, 'BLOOM_EDGE'))
+            G.add_nodes_from([i for i in range(n)])
+            G.add_edges_from(bloom_edges)
+        G = self._bloom_G
+        f, t = self._bloom_range
+        leaf = pds_id - f
+        print("Finding path from root to {} (pds_id {}) within {}".format(leaf, pds_id, self._bloom_fn))
+        # bloom tree uses 0 as the root
+        ids = nx.shortest_path(self._G, 0, leaf)
+        print("BLOOM IDs on the path {}".format(ids))
+        path = [self._bloom_nodes[node] for node in ids]
         return path[::-1]
 
 class ForestPathFinder(object):
@@ -113,7 +144,23 @@ class ForestPathFinder(object):
         nssc = len(self._ssc_files)
         nct = len(self._ct_files)
         assert nssc == nct, 'number of compact tree files ({}) should match number of sample set connectivity files ({})'.format(nct, nssc)
-        self._nroots = nssc
+        self._nroots = int(self._roots.shape[0])
+        assert self._nroots == nct, 'number of roots ({}) should match number of sample set connectivity files ({})'.format(self._nroots, nssc)
+        self._bloom_range = None
+        self._bloom_files = None
+        if args.bloom_dir is not None:
+            QB = _load(args.rootf, 'QB') # Q (configuration) Base
+            if QB is not None:
+                eot = int(QB[-1])
+                QB = QB.tolist()
+                QB.append(eot)
+                bloom_range = []
+                for f, t in zip(QB[:-1], QB[1:]):
+                    bloom_range.append([f,t])
+                bloom_range.append([QB[-1], self._nroots])
+                self._bloom_range = np.array(bloom_range) # N x 2 array of [from, to)
+                self._bloom_files = _lsv(args.bloom_dir, args.prefix_bloom, '.npz')
+                assert len(self._bloom_files) == self._nroots, "number of roots ({}) should match number of blooming files ({}). Maybe there is legacy files in bloom_dir?".format(self._nroots, len(self._bloom_files))
 
     def _solve_root_and_pds(self):
         G = self._G = nx.Graph()
@@ -148,6 +195,13 @@ class ForestPathFinder(object):
         #return
         G.add_edges_from(edges_1)
         G.add_edges_from(edges_2)
+        if self._bloom_range is not None:
+            for root in range(self._nroots):
+                f, t = self._bloom_range[root]
+                edges_3 = np.zeros((t - f, 2), dtype=np.int64)
+                edges_3[:,0] = -1 - root
+                edges_3[:,1] = np.array([i for i in range(f, t)])
+                G.add_edges_from(edges_3)
         # print(nx.shortest_path(G, -1, -1026))
         # -1: Root 0 (init), -2: Root 1 (goal)
         init_tree = -1
@@ -166,8 +220,12 @@ class ForestPathFinder(object):
                 virtual_edges.append((-1 - root, VIRTUAL_OPEN_SPACE_NODE))
             G.add_edges_from(virtual_edges)
             goal_tree = VIRTUAL_OPEN_SPACE_NODE
-        self._sssp = nx.shortest_path(G, init_tree, goal_tree)
-        print('Forest-level shortest path {}'.format(self._sssp))
+        try:
+            self._sssp = nx.shortest_path(G, init_tree, goal_tree)
+            print('Forest-level shortest path {}'.format(self._sssp))
+        except nx.exception.NetworkXNoPath:
+            return False
+        return True
 
     def _solve_in_single_tree(self, n_from, n_to):
         if isinstance(n_from, complex):
@@ -180,12 +238,16 @@ class ForestPathFinder(object):
             reverse = False
         tree = self._cache_lookup(root)
         if tree is None:
+            bloom_range = None if self._bloom_range is None else self._bloom_range[root]
+            bloom_fn = None if self._bloom_files is None else self._bloom_files[root]
             tree = TreePathFinder(root=self._roots[root],
                                   ssc_fn=self._ssc_files[root],
                                   ct_fn=self._ct_files[root],
                                   pds=self._pds,
                                   pds_ids=self._pds_ids,
-                                  pds_flags=self._pds_flags)
+                                  pds_flags=self._pds_flags,
+                                  bloom_range=bloom_range,
+                                  bloom_fn=bloom_fn)
         path = tree.nodelist_from_root(leaf)
         self._cache_tree(tree, root)
         if reverse:
@@ -203,7 +265,8 @@ class ForestPathFinder(object):
         self._cached_tree_root = root
 
     def solve(self):
-        self._solve_root_and_pds()
+        if not self._solve_root_and_pds():
+            return False
         # Sanity check!
         bugnode = []
         for root in self._sssp:
@@ -227,6 +290,7 @@ class ForestPathFinder(object):
             if traj is not None:
                 path = traj if path is None else np.concatenate((path, traj), axis=0)
         self._sssp_full = path
+        return True
 
     def output(self):
         print(self._pds_size)
@@ -245,12 +309,16 @@ def main():
     parser.add_argument('--forest_edge', help='Pre-processed edge file from pds_edge.py', required=True)
     parser.add_argument('--prefix_ct', help='File name prefix of compact tree file ', default='compact_tree-')
     parser.add_argument('--prefix_ssc', help='File name prefix of sample set connectivity (ssc) file', default='ssc-')
-    parser.add_argument('--bloom_base', help='File that store the bases of blooming trees in PDS', default=None)
+    # Prerequisites to use Blooming trees in forest_dijkstra.
+    # 1. QB array presents in --pdsf
+    # 2. --bloom_dir is specified
+    parser.add_argument('--bloom_dir', help='Directory of PDS blooming trees', default=None)
+    parser.add_argument('--prefix_bloom', help='File name prefix of blooming files', default='bloom-from_')
     parser.add_argument('--out', help='Output path file. default to stdout', default=None)
     args = parser.parse_args()
     fpf = ForestPathFinder(args)
-    fpf.solve()
-    fpf.output()
+    if fpf.solve():
+        fpf.output()
 
 if __name__ == '__main__':
     main()
