6486948268e3b4a7869fcd7f7e1f17920df79e52
Xinya Zhang <xinyazhang@utexas.edu>
2019-10-30 17:20:30 -0500

    GP/pipeline/condor: request 4G memory

--- src/GP/pipeline/condor.py
+++ src/GP/pipeline/condor.py
@@ -58,6 +58,7 @@ def local_submit(ws,
         print('Executable = {}'.format(xfile), file=f)
         print('environment = "OMP_NUM_THREADS=1"', file=f)
         print('getenv = True', file=f) # We need it because
+        print('request_memory = 4096', file=f) # 4G memory
         print('Output = {}/$(Process).out'.format(local_scratch), file=f)
         print('Error = {}/$(Process).err'.format(local_scratch), file=f)
         print('Log = {}/log'.format(local_scratch), file=f)
