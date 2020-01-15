2670be2cb1b41e6c292d46445956a3050a73f2d6
Xinya Zhang <xinyazhang@utexas.edu>
2020-01-15 17:31:40 -0600

    scripts/puzzle_visualization: update the camera for aj.

--- scripts/puzzle_visualization/blender_aj.sh
+++ scripts/puzzle_visualization/blender_aj.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ./facade.py tools blender --current_trial 21 condor.u3/aj --puzzle_name aj \
-	--camera_origin 15 -90 150 \
-	--camera_lookat 15 20 0 \
+	--camera_origin -200 40 220 \
+	--camera_lookat 10 40 0 \
 	--camera_up 0 0 1 \
 	--light_auto \
 	--image_frame 485 \
