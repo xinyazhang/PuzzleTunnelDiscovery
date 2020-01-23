74fcdd3eed33eeac3676db85684f90a833a73401
Xinya Zhang <xinyazhang@utexas.edu>
2020-01-23 02:07:00 -0600

    scripts/reproduce: record how we trained the network.

---
+++ scripts/reproduce/train_network.sh
@@ -0,0 +1,3 @@
+./facade.py train --stage train_both --nn_profile tag:256hg.+normal.hg1.feat128.+selective_piece.piece#0,1 --max_epoch 100 workspaces/joint-net-doublepiece/
+./facade.py train --stage train_both --nn_profile tag:256hg.+normal.hg1.feat128.+selective_piece.piece#2 --max_epoch 75 workspaces/joint-net-doublepiece/
+./facade.py train --stage train_both --nn_profile tag:256hg.+normal.hg1.feat128.+selective_piece.piece#3 --max_epoch 75 workspaces/joint-net-doublepiece/
