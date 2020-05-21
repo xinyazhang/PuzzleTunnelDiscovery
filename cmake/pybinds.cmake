dd5381863fa647c52fea5a24ae613112fc3ec1e8
Xinya Zhang <xinyazhang@utexas.edu>
2020-05-20 20:15:37 -0500

    build: do not build pyvistexture if USE_GPU is turned off

--- cmake/pybinds.cmake
+++ cmake/pybinds.cmake
@@ -8,7 +8,9 @@ use_fcl(pyse3ompl)
 PYADD(pygeokey geokey)
 add_dependencies(pygeokey ext_libgeokey)
 
+if (USE_GPU)
 PYADD(pyvistexture osr ${VISUAL_PACK})
 add_dependencies(pyvistexture osr)
+endif()
 
 PYADD(pycutec2)
